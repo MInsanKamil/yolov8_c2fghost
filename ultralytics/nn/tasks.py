4d0d6c7e905100a2199132ddfd6f755a975df1df
MInsanKamil <89583296+MInsanKamil@users.noreply.github.com>
2024-03-15 22:20:02 +0800

    fix

--- ultralytics/nn/tasks.py
+++ ultralytics/nn/tasks.py
@@ -7,9 +7,9 @@ from pathlib import Path
 import torch
 import torch.nn as nn
 
-# from ultralytics.nn.modules.conv import(
-# ,
-# )
+from ultralytics.nn.modules.conv import(
+LightConv,
+)
 from ultralytics.nn.modules.block import(
     C2fGhost,
 )
@@ -853,6 +853,7 @@ def parse_model(d, ch, verbose=True):  # model_dict, input_channels(3)
             Conv,
             ConvTranspose,
             GhostConv,
+            LightConv,
             Bottleneck,
             GhostBottleneck,
             SPP,
