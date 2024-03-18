bfb4888338826e2a58c2e81981f99231b568e2aa
MInsanKamil <89583296+MInsanKamil@users.noreply.github.com>
2024-03-18 16:44:18 +0800

    fix C2FADown

--- ultralytics/nn/modules/block.py
+++ ultralytics/nn/modules/block.py
@@ -314,11 +314,29 @@ class C2fGhost(C2f):
         self.m = nn.Sequential(*(GhostBottleneck(c_,c_,1,2) for _ in range(n)))
 
 class C2fADown(C2f):
-    def init(self,c1,c2,n=1,shortcut=False,g=1,e=0.5):
-        super().__init__(c1, c2, n, shortcut, g, e)
-        c = int(c2 * e)  # hidden channels
-        self.cv1 = ADown(c1, 2 * c)
-        self.cv2 = ADown((2 + n) * c, c2)
+    def __init__(self, c1, c2, n=1, shortcut=False, g=1, e=0.5):
+        """Initialize CSP bottleneck layer with two convolutions with arguments ch_in, ch_out, number, shortcut, groups,
+        expansion.
+        """
+        super().__init__()
+        self.c = int(c2 * e)  # hidden channels
+        self.cv1 = ADown(c1, 2 * self.c)
+        self.cv2 = ADown((2 + n) * self.c, c2)  # optional act=FReLU(c2)
+        self.m = nn.ModuleList(Bottleneck(self.c, self.c, shortcut, g, k=((3, 3), (3, 3)), e=1.0) for _ in range(n))
+
+    def forward(self, x):
+        """Forward pass through C2f layer."""
+        y = list(self.cv1(x).chunk(2, 1))
+        y.extend(m(y[-1]) for m in self.m)
+        return self.cv2(torch.cat(y, 1))
+        
+
+    def forward_split(self, x):
+        """Forward pass using split() instead of chunk()."""
+        y = list(self.cv1(x).split((self.c, self.c), 1))
+        y.extend(m(y[-1]) for m in self.m)
+        return self.cv2(torch.cat(y, 1))
+    
 
 
 
