92536a5c4a6c7d286e033ef9888c3cb99144f72a
MInsanKamil <89583296+MInsanKamil@users.noreply.github.com>
2024-03-21 06:52:54 +0800

    add avg pool

--- ultralytics/nn/modules/block.py
+++ ultralytics/nn/modules/block.py
@@ -311,6 +311,32 @@ class C2f(nn.Module):
         return self.cv2(torch.cat(y, 1))
     
 
+# class C2f_Double_Stride(nn.Module):
+#     """Faster Implementation of CSP Bottleneck with 2 convolutions."""
+
+#     def __init__(self, c1, c2, n=1, shortcut=False, g=1, e=0.5):
+#         """Initialize CSP bottleneck layer with two convolutions with arguments ch_in, ch_out, number, shortcut, groups,
+#         expansion.
+#         """
+#         super().__init__()
+#         self.c = int(c2 * e)  # hidden channels
+#         self.cv1 = Conv(c1, 2 * self.c, 1, 2)
+#         self.cv2 = Conv((2 + n) * self.c, c2, 1, 2)  # optional act=FReLU(c2)
+#         self.m = nn.ModuleList(Bottleneck(self.c, self.c, shortcut, g, k=((3, 3), (3, 3)), e=1.0) for _ in range(n))
+
+#     def forward(self, x):
+#         """Forward pass through C2f layer."""
+#         y = list(self.cv1(x).chunk(2, 1))
+#         y.extend(m(y[-1]) for m in self.m)
+#         return self.cv2(torch.cat(y, 1))
+        
+
+#     def forward_split(self, x):
+#         """Forward pass using split() instead of chunk()."""
+#         y = list(self.cv1(x).split((self.c, self.c), 1))
+#         y.extend(m(y[-1]) for m in self.m)
+#         return self.cv2(torch.cat(y, 1))
+    
 class C2f_Double_Stride(nn.Module):
     """Faster Implementation of CSP Bottleneck with 2 convolutions."""
 
@@ -320,22 +346,26 @@ class C2f_Double_Stride(nn.Module):
         """
         super().__init__()
         self.c = int(c2 * e)  # hidden channels
-        self.cv1 = Conv(c1, 2 * self.c, 1, 2)
-        self.cv2 = Conv((2 + n) * self.c, c2, 1, 2)  # optional act=FReLU(c2)
+        self.cv1 = Conv(c1, 2 * self.c, 1, 1)  
+        self.cv2 = Conv((2 + n) * self.c, c2, 1, 1) 
         self.m = nn.ModuleList(Bottleneck(self.c, self.c, shortcut, g, k=((3, 3), (3, 3)), e=1.0) for _ in range(n))
+        self.global_avg_pool = nn.AdaptiveAvgPool2d(1)  # GAP layer
 
     def forward(self, x):
         """Forward pass through C2f layer."""
         y = list(self.cv1(x).chunk(2, 1))
         y.extend(m(y[-1]) for m in self.m)
-        return self.cv2(torch.cat(y, 1))
-        
+        x = self.cv2(torch.cat(y, 1))
+        x = self.global_avg_pool(x)
+        return x.squeeze(2).squeeze(2)  # Flatten the output
 
     def forward_split(self, x):
         """Forward pass using split() instead of chunk()."""
         y = list(self.cv1(x).split((self.c, self.c), 1))
         y.extend(m(y[-1]) for m in self.m)
-        return self.cv2(torch.cat(y, 1))
+        x = self.cv2(torch.cat(y, 1))
+        x = self.global_avg_pool(x)
+        return x.squeeze(2).squeeze(2)  # Flatten the output
 
 class C2fGhost(nn.Module):
     def __init__(self, c1, c2, n=1, shortcut=False, g=1, e=0.5):
