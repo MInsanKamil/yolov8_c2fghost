052ec5181c623d68e6a6d7130e4facfaf2d4bfa6
MInsanKamil <89583296+MInsanKamil@users.noreply.github.com>
2024-03-21 23:44:29 +0800

    add c3f_double_stride

--- ultralytics/nn/modules/block.py
+++ ultralytics/nn/modules/block.py
@@ -28,6 +28,7 @@ __all__ = (
     "C2f_Stride",
     "C2f_Upsample",
     "C2f_Stride_Maxpool",
+    "C3f_Double_Stride",
     "ImagePoolingAttn",
     "ContrastiveHead",
     "BNContrastiveHead",
@@ -345,6 +346,37 @@ class C2f(nn.Module):
         y = list(self.cv1(x).split((self.c, self.c), 1))
         y.extend(m(y[-1]) for m in self.m)
         return self.cv2(torch.cat(y, 1))
+    
+class C3f_Double_Stride(nn.Module):
+    """Faster Implementation of CSP Bottleneck with 2 convolutions."""
+
+    def __init__(self, c1, c2, n=1, shortcut=False, g=1, e=0.5):
+        """Initialize CSP bottleneck layer with two convolutions with arguments ch_in, ch_out, number, shortcut, groups,
+        expansion.
+        """
+        super().__init__()
+        self.c = int(c2 * e)  # hidden channels
+        self.cv1 = Conv(c1, 2 * self.c, 1, 1)
+        self.cv2 = Conv((2 + n) * self.c, c2, 2)
+        self.cv3 = Conv(c1, 2 * self.c, 1, 2)  # optional act=FReLU(c2)
+        self.m = nn.ModuleList(Bottleneck(self.c, self.c, shortcut, g, k=((3, 3), (3, 3)), e=1.0) for _ in range(n))
+
+    def forward(self, x):
+        """Forward pass through C2f layer."""
+        y = list(self.cv1(x).chunk(2, 1))
+        y.extend(m(y[-1]) for m in self.m)
+        x = self.cv2(torch.cat(y, 1))
+        x = self.cv3(x)
+        return x
+        
+
+    def forward_split(self, x):
+        """Forward pass using split() instead of chunk()."""
+        y = list(self.cv1(x).split((self.c, self.c), 1))
+        y.extend(m(y[-1]) for m in self.m)
+        x = self.cv2(torch.cat(y, 1))
+        x = self.cv3(x)
+        return x
     
 
 # class C2f_Double_Stride(nn.Module):
