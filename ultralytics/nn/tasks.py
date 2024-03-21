146acd697e5e0892a997ef85a257747bd20e8096
MInsanKamil <89583296+MInsanKamil@users.noreply.github.com>
2024-03-22 00:10:01 +0800

    fix

--- ultralytics/nn/tasks.py
+++ ultralytics/nn/tasks.py
@@ -11,7 +11,7 @@ from ultralytics.nn.modules.conv import(
 LightConv,Conv_Max_Pooling
 )
 from ultralytics.nn.modules.block import(
-    C2fADown,C2fGhost,C2fAttnGhost,C2f_Double_Stride,C2f_Stride,C2f_MaxPool, C2f_Double_MaxPool, C2f_Upsample,C2f_Stride_Maxpool,C3f_Double_Stride
+    C2fADown,C2fGhost,C2fAttnGhost,C2f_Double_Stride,C2f_Stride,C2f_MaxPool, C2f_Double_MaxPool, C2f_Upsample,C2f_Stride_Maxpool,C2f_Fractional_MaxPool,C3f_Double_Stride
 )
 
 from ultralytics.nn.modules import (
@@ -874,6 +874,7 @@ def parse_model(d, ch, verbose=True):  # model_dict, input_channels(3)
             C2f_Double_MaxPool,
             C2f_Upsample,
             C2f_Stride_Maxpool,
+            C2f_Fractional_MaxPool,
             C3f_Double_Stride,
             RepNCSPELAN4,
             ADown,
@@ -897,7 +898,7 @@ def parse_model(d, ch, verbose=True):  # model_dict, input_channels(3)
                 )  # num heads
 
             args = [c1, c2, *args[1:]]
-            if m in (BottleneckCSP, C1, C2, C2f, C2fAttn, C3, C3TR, C3Ghost,C2fADown,C2fGhost,C2fAttnGhost,C2f_Double_Stride,C2f_Stride,C2f_MaxPool,C2f_Double_MaxPool,C2f_Upsample,C2f_Stride_Maxpool,C3f_Double_Stride, C3x, RepC3):
+            if m in (BottleneckCSP, C1, C2, C2f, C2fAttn, C3, C3TR, C3Ghost,C2fADown,C2fGhost,C2fAttnGhost,C2f_Double_Stride,C2f_Stride,C2f_MaxPool,C2f_Double_MaxPool,C2f_Upsample,C2f_Stride_Maxpool,C2f_Fractional_MaxPool,C3f_Double_Stride, C3x, RepC3):
                 args.insert(2, n)  # number of repeats
                 n = 1
         elif m is AIFI:
