bd9fb4645257a92a96c1f70a99812b66c9936094
MInsanKamil <89583296+MInsanKamil@users.noreply.github.com>
2024-04-12 23:32:20 +0800

    SPPF_Fractional

--- ultralytics/nn/modules/block.py
+++ ultralytics/nn/modules/block.py
@@ -201,7 +201,7 @@ class SPPF_Fractional(nn.Module):
         c_ = c1 // 2  # hidden channels
         self.cv1 = Conv(c1, c_, 1, 1)
         self.cv2 = Conv(c_ * 4, c2, 1, 1)
-        self.m = nn.FractionalMaxPool2d(kernel_size=k, output_size=self.cv1.shape())
+        self.m = nn.FractionalMaxPool2d(kernel_size=k, output_ratio=(0, 1))
 
     def forward(self, x):
         """Forward pass through Ghost Convolution block."""
