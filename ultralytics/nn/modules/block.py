ff14271a3738e12f0b60083e3de1a6e779b23933
MInsanKamil <89583296+MInsanKamil@users.noreply.github.com>
2024-04-12 22:12:28 +0800

    SPPF_Fractional

--- ultralytics/nn/modules/block.py
+++ ultralytics/nn/modules/block.py
@@ -14,6 +14,7 @@ __all__ = (
     "HGStem",
     "SPP",
     "SPPF",
+    "SPPF_Fractional",
     "C1",
     "C2",
     "C3",
@@ -187,6 +188,27 @@ class SPPF(nn.Module):
         y2 = self.m(y1)
         return self.cv2(torch.cat((x, y1, y2, self.m(y2)), 1))
 
+class SPPF_Fractional(nn.Module):
+    """Spatial Pyramid Pooling - Fast (SPPF) layer for YOLOv5 by Glenn Jocher."""
+
+    def __init__(self, c1, c2, k=5):
+        """
+        Initializes the SPPF layer with given input/output channels and kernel size.
+
+        This module is equivalent to SPP(k=(5, 9, 13)).
+        """
+        super().__init__()
+        c_ = c1 // 2  # hidden channels
+        self.cv1 = Conv(c1, c_, 1, 1)
+        self.cv2 = Conv(c_ * 4, c2, 1, 1)
+        self.m = nn.FractionalMaxPool2d(kernel_size=k, output_ratio=(1, 1))
+
+    def forward(self, x):
+        """Forward pass through Ghost Convolution block."""
+        x = self.cv1(x)
+        y1 = self.m(x)
+        y2 = self.m(y1)
+        return self.cv2(torch.cat((x, y1, y2, self.m(y2)), 1))
 
 class C1(nn.Module):
     """CSP Bottleneck with 1 convolution."""
