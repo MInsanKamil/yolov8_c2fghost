bafadf513ee1d1826a589b4e1d32f280c2ea23a4
MInsanKamil <89583296+MInsanKamil@users.noreply.github.com>
2024-04-14 21:28:22 +0800

    fix

--- ultralytics/nn/modules/conv.py
+++ ultralytics/nn/modules/conv.py
@@ -268,12 +268,14 @@ class Conv_Fractional_Max_Pooling(nn.Module):
 
     def forward(self, x):
         """Apply convolution, batch normalization and activation to input tensor."""
+        x = self.fractional_max_pool(x)
         x = self.act(self.bn(self.conv(x)))
         x = self.fractional_max_pool(x)
         return x
 
     def forward_fuse(self, x):
         """Perform transposed convolution of 2D data."""
+        x = self.fractional_max_pool(x)
         x = self.act(self.conv(x))
         x = self.fractional_max_pool(x)
         return x
