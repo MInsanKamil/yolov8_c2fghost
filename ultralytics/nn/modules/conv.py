5cbdc854b473c558f9cd61569d5df58410aac1e1
MInsanKamil <89583296+MInsanKamil@users.noreply.github.com>
2024-04-16 21:17:35 +0800

    attantion exp

--- ultralytics/nn/modules/conv.py
+++ ultralytics/nn/modules/conv.py
@@ -290,7 +290,7 @@ class Conv_SP(nn.Module):
         self.conv = nn.Conv2d(c1, c2, k, s, autopad(k, p, d), groups=g, dilation=d, bias=False)
         self.bn = nn.BatchNorm2d(c2)
         self.act = self.default_act if act is True else act if isinstance(act, nn.Module) else nn.Identity()
-        self.sattn = SpatialAttention()
+        self.sattn = SpatialAttention(kernel_size=3)
 
     def forward(self, x):
         """Apply convolution, batch normalization and activation to input tensor."""
