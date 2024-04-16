f53a00af4dd6eec93eb382f64890aa291119e2c9
MInsanKamil <89583296+MInsanKamil@users.noreply.github.com>
2024-04-16 12:43:03 +0800

    attantion exp

--- ultralytics/nn/modules/conv.py
+++ ultralytics/nn/modules/conv.py
@@ -291,7 +291,7 @@ class Conv_Fractional_Max_Pooling_Attn(nn.Module):
         self.bn = nn.BatchNorm2d(c2)
         self.act = self.default_act if act is True else act if isinstance(act, nn.Module) else nn.Identity()
         self.fractional_max_pool = nn.FractionalMaxPool2d(3, output_ratio=(0.25, 0.25))
-        self.attn = SpatialAttention(kernel_size=3)
+        self.attn = CBAM(c1, kernel_size=3)
 
     def forward(self, x):
         """Apply convolution, batch normalization and activation to input tensor."""
