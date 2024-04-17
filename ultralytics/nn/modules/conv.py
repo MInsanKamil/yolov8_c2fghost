ab53cbd0b46ac154af631880257eafc661c4f34d
MInsanKamil <89583296+MInsanKamil@users.noreply.github.com>
2024-04-17 23:34:25 +0800

    ratio exp

--- ultralytics/nn/modules/conv.py
+++ ultralytics/nn/modules/conv.py
@@ -11,6 +11,7 @@ __all__ = (
     "Conv",
     "Conv2",
     "LightConv",
+    "CBAM_Conv_Fractional_Max_Pooling",
     "Conv_SP",
     "DWConv",
     "DWConvTranspose2d",
@@ -275,6 +276,34 @@ class Conv_Fractional_Max_Pooling(nn.Module):
 
     def forward_fuse(self, x):
         """Perform transposed convolution of 2D data."""
+        x = self.act(self.conv(x))
+        x = self.fractional_max_pool(x)
+        return x
+    
+class CBAM_Conv_Fractional_Max_Pooling(nn.Module):
+    """Standard convolution with args(ch_in, ch_out, kernel, stride, padding, groups, dilation, activation)."""
+
+    default_act = nn.SiLU()  # default activation
+
+    def __init__(self, c1, c2, k=1, s=1, p=None, g=1, d=1, act=True):
+        """Initialize Conv layer with given arguments including activation."""
+        super().__init__()
+        self.conv = nn.Conv2d(c1, c2, k, s, autopad(k, p, d), groups=g, dilation=d, bias=False)
+        self.bn = nn.BatchNorm2d(c2)
+        self.act = self.default_act if act is True else act if isinstance(act, nn.Module) else nn.Identity()
+        self.fractional_max_pool = nn.FractionalMaxPool2d(3, output_ratio=(0.25, 0.25))
+        self.cbam = CBAM(c1)
+
+    def forward(self, x):
+        """Apply convolution, batch normalization and activation to input tensor."""
+        x = self.cbam(x)
+        x = self.act(self.bn(self.conv(x)))
+        x = self.fractional_max_pool(x)
+        return x
+
+    def forward_fuse(self, x):
+        """Perform transposed convolution of 2D data."""
+        x = self.cbam(x)
         x = self.act(self.conv(x))
         x = self.fractional_max_pool(x)
         return x
