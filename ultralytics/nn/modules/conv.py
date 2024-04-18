1d62b730c7329f9dc7ff7fbf718890e07addbe82
MInsanKamil <89583296+MInsanKamil@users.noreply.github.com>
2024-04-18 11:40:05 +0800

    s3pool

--- ultralytics/nn/modules/conv.py
+++ ultralytics/nn/modules/conv.py
@@ -6,12 +6,14 @@ import math
 import numpy as np
 import torch
 import torch.nn as nn
+from torch.autograd import Variable
 
 __all__ = (
     "Conv",
     "Conv2",
     "LightConv",
     "CBAM_Conv_Fractional_Max_Pooling",
+    "Conv_S3Pool_Attn",
     "Conv_SP",
     "DWConv",
     "DWConvTranspose2d",
@@ -332,7 +334,99 @@ class Conv_SP(nn.Module):
         x = self.act(self.conv(x))
         x = self.sattn(x)
         return x
+class StochasticPool2DLayer(nn.Module):
+    def __init__(self, pool_size=2, maxpool=True, training=False, grid_size=None, **kwargs):
+        super(StochasticPool2DLayer, self).__init__(**kwargs)
+        self.rng = torch.cuda.manual_seed_all(123) # this changed in Pytorch for working
+        self.pool_size = pool_size
+        self.maxpool_flag = maxpool
+        self.training = training
+        if grid_size:
+            self.grid_size = grid_size
+        else:
+            self.grid_size = pool_size
 
+        self.Maxpool = torch.nn.MaxPool2d(kernel_size=self.pool_size, stride=1)
+        self.Avgpool = torch.nn.AvgPool2d(kernel_size=self.pool_size,
+                                          stride=self.pool_size,
+                                          padding=self.pool_size//2,)
+        self.padding = nn.ConstantPad2d((0,1,0,1),0)
+
+    def forward(self, x, **kwargs):
+        if self.maxpool_flag:
+            x = self.Maxpool(x)
+            x = self.padding(x)
+        if not self.training:
+            # print(x.size())
+            x = self.Avgpool(x)
+            return x
+            # return x[:, :, ::self.pool_size, ::self.pool_size]       
+        else:
+            w, h = x.data.shape[2:]
+            n_w, n_h = w//self.grid_size, h//self.grid_size
+            n_sample_per_grid = self.grid_size//self.pool_size
+            # print('===========================')
+            idx_w = []
+            idx_h = []
+            if w>2 and h>2:
+                for i in range(n_w):
+                    offset = self.grid_size * i
+                    if i < n_w - 1:
+                        this_n = self.grid_size
+                    else:
+                        this_n = x.data.shape[2] - offset
+                    
+                    this_idx, _ = torch.sort(torch.randperm(this_n)[:n_sample_per_grid])
+                    idx_w.append(offset + this_idx)
+                for i in range(n_h):
+                    offset = self.grid_size * i
+                    if i < n_h - 1:
+                        this_n = self.grid_size
+                    else:
+                        this_n = x.data.shape[3] - offset
+                    this_idx, _ = torch.sort(torch.randperm(this_n)[:n_sample_per_grid])
+
+                    idx_h.append(offset + this_idx)
+                idx_w = torch.cat(idx_w, dim=0)
+                idx_h = torch.cat(idx_h, dim=0)
+            else:
+                idx_w = torch.LongTensor([0])
+                idx_h = torch.LongTensor([0])
+
+            output = x[:, :, idx_w.cpu()][:, :, :, idx_h.cpu()]
+            return output
+        
+class Conv_S3Pool_Attn(nn.Module):
+    """Standard convolution with args(ch_in, ch_out, kernel, stride, padding, groups, dilation, activation)."""
+
+    default_act = nn.SiLU()  # default activation
+
+    def __init__(self, c1, c2, k=1, s=1, p=None, g=1, d=1, act=True):
+        """Initialize Conv layer with given arguments including activation."""
+        super().__init__()
+        self.conv = nn.Conv2d(c1, c2, k, s, autopad(k, p, d), groups=g, dilation=d, bias=False)
+        self.bn = nn.BatchNorm2d(c2)
+        self.act = self.default_act if act is True else act if isinstance(act, nn.Module) else nn.Identity()
+        self.s3pool = StochasticPool2DLayer(pool_size=4, maxpool=True)
+        self.cattn = ChannelAttention(c1)
+        self.sattn = SpatialAttention(kernel_size=7)
+
+    def forward(self, x):
+        """Apply convolution, batch normalization and activation to input tensor."""
+        x = self.cattn(x)
+        x = self.act(self.bn(self.conv(x)))
+        x = self.s3pool(x)
+        x = self.sattn(x)
+        return x
+
+    def forward_fuse(self, x):
+        """Perform transposed convolution of 2D data."""
+        x = self.cattn(x)
+        x = self.act(self.conv(x))
+        x = self.s3pool(x)
+        x = self.sattn(x)
+        return x
+    
 class Conv_Fractional_Max_Pooling_Attn(nn.Module):
     """Standard convolution with args(ch_in, ch_out, kernel, stride, padding, groups, dilation, activation)."""
 
