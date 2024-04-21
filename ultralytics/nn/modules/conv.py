a7ca16f7e702acd93c230338a664650c9499e201
MInsanKamil <89583296+MInsanKamil@users.noreply.github.com>
2024-04-21 12:44:27 +0800

    fix attn

--- ultralytics/nn/modules/conv.py
+++ ultralytics/nn/modules/conv.py
@@ -545,22 +545,22 @@ class Conv_Fractional_Max_Pooling_Attn(nn.Module):
         self.bn = nn.BatchNorm2d(c2)
         self.act = self.default_act if act is True else act if isinstance(act, nn.Module) else nn.Identity()
         self.fractional_max_pool = nn.FractionalMaxPool2d(3, output_ratio=(0.25, 0.25))
-        self.cattn = ChannelAttention(c2)
+        self.cattn = ChannelAttention(c1)
         self.sattn = SpatialAttention(kernel_size=7)
 
     def forward(self, x):
         """Apply convolution, batch normalization and activation to input tensor."""
+        x = self.cattn(x)
         x = self.act(self.bn(self.conv(x)))
         x = self.fractional_max_pool(x)
-        x = self.cattn(x)
         x = self.sattn(x)
         return x
 
     def forward_fuse(self, x):
         """Perform transposed convolution of 2D data."""
+        x = self.cattn(x)
         x = self.act(self.conv(x))
         x = self.fractional_max_pool(x)
-        x = self.cattn(x)
         x = self.sattn(x)
         return x
     
