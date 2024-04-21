362f7164f150d85a037f5cb2eebb8f3fc4ab36dc
MInsanKamil <89583296+MInsanKamil@users.noreply.github.com>
2024-04-21 12:05:57 +0800

    fix attn

--- predict_yolov8.py
+++ predict_yolov8.py
@@ -1,10 +1,10 @@
 import cv2
 import argparse
-from ultralytics.models.yolo.model import YOLO
+from ultralytics import YOLO
 # from ultralytics import YOLO
 import supervision as sv #must be version 0.3.0
 import time 
-from picamera2 import Picamera2
+# from picamera2 import Picamera2
 
 def parse_arguments() -> argparse.Namespace:
     parser = argparse.ArgumentParser(description = "YOLOv8 Live")
@@ -21,9 +21,9 @@ def parse_arguments() -> argparse.Namespace:
 def main():
     args = parse_arguments()
     frame_width, frame_height = args.webcam_resolution
-    cap = Picamera2(0)
-    cap.configure(cap.create_preview_configuration(main={"format": "XRGB8888", "size": (frame_width, frame_height)}))
-    cap.start()
+    cap = cv2.VideoCapture(0)
+    cap.set(cv2.CAP_PROP_FRAME_WIDTH,frame_width)
+    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, frame_height)
     model = YOLO("best.pt")
     prev_frame_time = 0
     new_frame_time = 0
@@ -38,33 +38,33 @@ def main():
 
     while True:
         new_frame_time = time.time() 
-        frame = cap.capture_array()
+        ret, frame = cap.read()
 
-        if frame.shape[2] == 4:
-            frame = frame[:, :, :3]
-        
-        result = model(frame, conf = 0.5, agnostic_nms = True)[0]
-        detections = sv.Detections.from_ultralytics(result)
-        fps = 1/(new_frame_time-prev_frame_time) 
-        prev_frame_time = new_frame_time 
-        labels = [
-            f"{model.model.names[class_id]} {confidence:0.2f}"
-        for _, _, confidence, class_id, tracker_id,_
-        in detections
-    ]
+        # if frame.shape[2] == 4:
+        #     frame = frame[:, :, :3]
+        if ret:
+            result = model(frame, agnostic_nms = True)[0]
+            detections = sv.Detections.from_ultralytics(result)
+            fps = 1/(new_frame_time-prev_frame_time) 
+            prev_frame_time = new_frame_time 
+            labels = [
+                f"{model.model.names[class_id]} {confidence:0.2f}"
+            for _, _, confidence, class_id, tracker_id,_
+            in detections
+        ]
 
-        frame = box_annotator.annotate(
-        scene=frame,
-        detections=detections,
-        labels = labels
-        )
-        cv2.putText(frame, str(int(fps)), (7, 70), cv2.FONT_HERSHEY_SIMPLEX, 3, (100, 255, 0), 3, cv2.LINE_AA) 
-        cv2.imshow('yolov8', frame)
+            frame = box_annotator.annotate(
+            scene=frame,
+            detections=detections,
+            labels = labels
+            )
+            cv2.putText(frame, str(int(fps)), (7, 70), cv2.FONT_HERSHEY_SIMPLEX, 3, (100, 255, 0), 3, cv2.LINE_AA) 
+            cv2.imshow('yolov8', frame)
 
-        if (cv2.waitKey(30) == 27): #escape key
-            break
+            if (cv2.waitKey(30) == 27): #escape key
+                break
 
-        print(frame.shape)
+            print(frame.shape)
 
 if __name__ == "__main__":
     main()
