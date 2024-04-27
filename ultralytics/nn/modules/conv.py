574b617899d417b9d9629b2f6de4dabb2946c172
MInsanKamil <89583296+MInsanKamil@users.noreply.github.com>
2024-04-27 11:12:43 +0800

    fix

--- ultralytics/nn/modules/conv.py
+++ ultralytics/nn/modules/conv.py
@@ -224,7 +224,6 @@ class Conv_Prune(nn.Module):
         """Initialize Conv layer with given arguments including activation."""
         super().__init__()
         self.conv = prune.l1_unstructured(nn.Conv2d(c1, c2, k, s, autopad(k, p, d), groups=g, dilation=d, bias=False), name="weight", amount=0.5)
-        self.prune = torch.nn.utils.remove_weight_norm(self.conv , name='weight')
         self.bn = nn.BatchNorm2d(c2)
         self.act = self.default_act if act is True else act if isinstance(act, nn.Module) else nn.Identity()
 
@@ -235,11 +234,11 @@ class Conv_Prune(nn.Module):
 
     def forward(self, x):
         """Apply convolution, batch normalization and activation to input tensor."""
-        return self.act(self.bn(self.prune(x)))
+        return self.act(self.bn(self.conv(x)))
 
     def forward_fuse(self, x):
         """Perform transposed convolution of 2D data."""
-        return self.act(self.prune(x))
+        return self.act(self.conv(x))
     
 class Conv_3(nn.Module):
     """Standard convolution with args(ch_in, ch_out, kernel, stride, padding, groups, dilation, activation)."""
