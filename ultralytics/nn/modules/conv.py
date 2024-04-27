6ddcfa946f4755f5f3b7fcc2c18fbaf1d4e1a429
MInsanKamil <89583296+MInsanKamil@users.noreply.github.com>
2024-04-27 08:14:54 +0800

    prunning

--- ultralytics/nn/modules/conv.py
+++ ultralytics/nn/modules/conv.py
@@ -13,6 +13,7 @@ __all__ = (
     "Conv",
     "Conv2",
     "LightConv",
+    "Conv_Prune",
     "CBAM_Conv_Fractional_Max_Pooling",
     "Conv_S3Pool_Attn",
     "Conv_SP",
@@ -213,6 +214,26 @@ class Conv(nn.Module):
         """Perform transposed convolution of 2D data."""
         return self.act(self.conv(x))
     
+class Conv_Prune(nn.Module):
+    """Standard convolution with args(ch_in, ch_out, kernel, stride, padding, groups, dilation, activation)."""
+
+    default_act = nn.SiLU()  # default activation
+
+    def __init__(self, c1, c2, k=1, s=1, p=None, g=1, d=1, act=True):
+        """Initialize Conv layer with given arguments including activation."""
+        super().__init__()
+        self.conv = nn.utils.prune.l1_unstructured(nn.Conv2d(c1, c2, k, s, autopad(k, p, d), groups=g, dilation=d, bias=False), name="weight", amount=0.2)
+        self.bn = nn.BatchNorm2d(c2)
+        self.act = self.default_act if act is True else act if isinstance(act, nn.Module) else nn.Identity()
+
+    def forward(self, x):
+        """Apply convolution, batch normalization and activation to input tensor."""
+        return self.act(self.bn(self.conv(x)))
+
+    def forward_fuse(self, x):
+        """Perform transposed convolution of 2D data."""
+        return self.act(self.conv(x))
+    
 class Conv_3(nn.Module):
     """Standard convolution with args(ch_in, ch_out, kernel, stride, padding, groups, dilation, activation)."""
 
@@ -250,6 +271,8 @@ class Conv_Max_Pooling(nn.Module):
         self.bn = nn.BatchNorm2d(c2)
         self.act = self.default_act if act is True else act if isinstance(act, nn.Module) else nn.Identity()
         self.max_pool = nn.MaxPool2d(3, stride=2)  # GAP layer
+        
+        
 
     def forward(self, x):
         """Apply convolution, batch normalization and activation to input tensor."""
