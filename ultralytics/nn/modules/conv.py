7860a4d14b153ad233ceff6838009e4a820060a6
MInsanKamil <89583296+MInsanKamil@users.noreply.github.com>
2024-04-27 09:08:59 +0800

    prunning

--- ultralytics/nn/modules/conv.py
+++ ultralytics/nn/modules/conv.py
@@ -223,7 +223,8 @@ class Conv_Prune(nn.Module):
     def __init__(self, c1, c2, k=1, s=1, p=None, g=1, d=1, act=True):
         """Initialize Conv layer with given arguments including activation."""
         super().__init__()
-        self.conv = prune.l1_unstructured(nn.Conv2d(c1, c2, k, s, autopad(k, p, d), groups=g, dilation=d, bias=False), name="weight", amount=0.2)
+        with torch.no_grad():
+            self.conv = prune.l1_unstructured(nn.Conv2d(c1, c2, k, s, autopad(k, p, d), groups=g, dilation=d, bias=False), name="weight", amount=0.2)
         self.bn = nn.BatchNorm2d(c2)
         self.act = self.default_act if act is True else act if isinstance(act, nn.Module) else nn.Identity()
 
