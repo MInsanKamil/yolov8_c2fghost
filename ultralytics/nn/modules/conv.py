0f73081a5f3238c6e14d0972eb2c08869d4a32b5
MInsanKamil <89583296+MInsanKamil@users.noreply.github.com>
2024-05-02 21:13:08 +0800

    cbam

--- ultralytics/nn/modules/conv.py
+++ ultralytics/nn/modules/conv.py
@@ -14,6 +14,7 @@ __all__ = (
     "Conv",
     "Conv2",
     "LightConv",
+    "CBAM_Conv_Avg_Pooling",
     "Conv_sliceSamp_Attn",
     "Conv_Spatial_Attn",
     "Conv_Prune",
@@ -468,6 +469,34 @@ class Conv_Avg_Pooling_Attn(nn.Module):
         x = self.sa(x)
         return x
     
+class CBAM_Conv_Avg_Pooling(nn.Module):
+    """Standard convolution with args(ch_in, ch_out, kernel, stride, padding, groups, dilation, activation)."""
+
+    default_act = nn.SiLU()  # default activation
+
+    def __init__(self, c1, c2, k=1, s=1, p=None, g=1, d=1, act=True):
+        """Initialize Conv layer with given arguments including activation."""
+        super().__init__()
+        self.conv = nn.Conv2d(c1, c2, k, s, autopad(k, p, d), groups=g, dilation=d, bias=False)
+        self.bn = nn.BatchNorm2d(c2)
+        self.act = self.default_act if act is True else act if isinstance(act, nn.Module) else nn.Identity()
+        self.avg_pool = nn.AvgPool2d(3, stride=2)  # GAP layer
+        self.cbam = CBAM(c1)
+
+    def forward(self, x):
+        """Apply convolution, batch normalization and activation to input tensor."""
+        x = self.cbam(x)
+        x = self.act(self.bn(self.conv(x)))
+        x = self.avg_pool(x)
+        return x
+
+    def forward_fuse(self, x):
+        """Perform transposed convolution of 2D data."""
+        x = self.cbam(x)
+        x = self.act(self.conv(x))
+        x = self.avg_pool(x)
+        return x
+    
 class Conv_Avg_Pooling_Spatial_Attn(nn.Module):
     """Standard convolution with args(ch_in, ch_out, kernel, stride, padding, groups, dilation, activation)."""
 
@@ -702,18 +731,18 @@ class CBAM_Conv(nn.Module):
         self.conv = nn.Conv2d(c1, c2, k, s, autopad(k, p, d), groups=g, dilation=d, bias=False)
         self.bn = nn.BatchNorm2d(c2)
         self.act = self.default_act if act is True else act if isinstance(act, nn.Module) else nn.Identity()
-        self.cbam = CBAM(c2)
+        self.cbam = CBAM(c1)
 
     def forward(self, x):
         """Apply convolution, batch normalization and activation to input tensor."""
-        x = self.act(self.bn(self.conv(x)))
         x = self.cbam(x)
+        x = self.act(self.bn(self.conv(x)))
         return x
 
     def forward_fuse(self, x):
         """Perform transposed convolution of 2D data."""
-        x = self.act(self.conv(x))
         x = self.cbam(x)
+        x = self.act(self.conv(x))
         return x
     
 class Conv_SP(nn.Module):
