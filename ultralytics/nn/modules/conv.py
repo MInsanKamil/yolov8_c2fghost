f0d7845c927f1c9394b65501a0ccb5710e0d362f
MInsanKamil <89583296+MInsanKamil@users.noreply.github.com>
2024-06-02 12:48:18 +0800

    ghostnet

--- ultralytics/nn/modules/conv.py
+++ ultralytics/nn/modules/conv.py
@@ -301,9 +301,9 @@ class GhostConv_Modification(nn.Module):
         activation.
         """
         super().__init__()
-        c_ = c2 // 4  # hidden channels
+        c_ = c2 // 2  # hidden channels
         self.cv1 = Conv(c1, c_, k, s, None, g, act=act)
-        # self.cv2 = Conv(c1, c_, k, s, None, g, act=act)
+        self.cv2 = Conv(c_, c_//4, 1, 1, None, c_//4, act=act)
         self.a = nn.AvgPool2d(3, 2, 1)
         self.fr = nn. FractionalMaxPool2d(3, output_ratio=(0.5, 0.5))
         self.m = nn.MaxPool2d(3, 2, 1)
@@ -312,7 +312,7 @@ class GhostConv_Modification(nn.Module):
     def forward(self, x):
         """Forward propagation through a Ghost Bottleneck layer with skip connection."""
         y = self.cv1(x)
-        return torch.cat((y, self.u(self.cv1(self.m(x))), self.u(self.cv1(self.fr(x))), self.u(self.cv1(self.a(x)))), 1)
+        return torch.cat((y,self.cv2(y), self.u(self.cv2(self.m(x))), self.u(self.cv2(self.fr(x))), self.u(self.cv2(self.a(x)))), 1)
 
 class GhostConv_Attn_Avg_Pool(nn.Module):
     """Ghost Convolution https://github.com/huawei-noah/ghostnet."""
