8e14c30b1c303dc96095010bf14ceec5f774f71a
MInsanKamil <89583296+MInsanKamil@users.noreply.github.com>
2024-06-02 20:26:39 +0800

    ghostnet

--- ultralytics/nn/modules/conv.py
+++ ultralytics/nn/modules/conv.py
@@ -387,7 +387,7 @@ class Conv_Prune(nn.Module):
     def __init__(self, c1, c2, k=1, s=1, p=None, g=1, d=1, act=True):
         """Initialize Conv layer with given arguments including activation."""
         super().__init__()
-        self.conv = prune.ln_structured(nn.Conv2d(c1, c2, k, s, autopad(k, p, d), groups=1, dilation=d, bias=False), name="weight", amount=0.01, dim=1, n=float('-inf'))
+        self.conv = prune.ln_structured(nn.Conv2d(c1, c2, k, s, autopad(k, p, d), groups=1, dilation=d, bias=False), name="weight", amount=0.2, dim=1, n=float('-inf'))
         self.bn = nn.BatchNorm2d(c2)
         self.act = self.default_act if act is True else act if isinstance(act, nn.Module) else nn.Identity()
 
