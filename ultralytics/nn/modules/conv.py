e7afdb475a1a76ac57b7bd09eb1f44979f3bd0da
MInsanKamil <89583296+MInsanKamil@users.noreply.github.com>
2024-06-03 17:37:01 +0800

    modify architecture

--- ultralytics/nn/modules/conv.py
+++ ultralytics/nn/modules/conv.py
@@ -1314,12 +1314,13 @@ class ChannelAttention_Pool(nn.Module):
         """Initializes the class and sets the basic configurations and instance variables required."""
         super().__init__()
         self.pool = nn.AvgPool2d(3, 2, 1)
+        self.pool1= nn.AdaptiveAvgPool2d(1)
         self.fc = nn.Conv2d(channels, channels, 1, 1, 0, bias=True)
         self.act = nn.Sigmoid()
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         """Applies forward pass using activation on convolutions of the input, optionally using batch normalization."""
-        return x * self.act(self.fc(self.pool(x)))
+        return self.pool(x) * self.act(self.fc(self.pool1(self.pool(x))))
 
 
 class SpatialAttention(nn.Module):
