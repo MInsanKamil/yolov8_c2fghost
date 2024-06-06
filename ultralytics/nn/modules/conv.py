16d6951bdbf1096d6698a09a63bfbdcbeea9c026
MInsanKamil <89583296+MInsanKamil@users.noreply.github.com>
2024-06-06 09:36:47 +0800

    modify architecture

--- ultralytics/nn/modules/conv.py
+++ ultralytics/nn/modules/conv.py
@@ -1343,7 +1343,7 @@ class ChannelAttention_Pool(nn.Module):
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         """Applies forward pass using activation on convolutions of the input, optionally using batch normalization."""
-        return x * self.act(self.fc(self.pool(x)))
+        return self.act(self.fc(self.pool(x)))
 
 
 class SpatialAttention(nn.Module):
