2d15465b602d83af20be1e8cfae873791e9b7de9
MInsanKamil <89583296+MInsanKamil@users.noreply.github.com>
2024-06-09 17:09:54 +0800

    Down Attn

--- ultralytics/nn/modules/conv.py
+++ ultralytics/nn/modules/conv.py
@@ -618,7 +618,7 @@ class Conv_DownSampleAttn(nn.Module):
         self.conv = nn.Conv2d(c1, c2, k, s, autopad(k, p, d), groups=g, dilation=d, bias=False)
         self.bn = nn.BatchNorm2d(c2)
         self.act = self.default_act if act is True else act if isinstance(act, nn.Module) else nn.Identity()
-        self.avg_pool = nn.AvgPool2d(3, stride=2)  # GAP layer
+        self.avg_pool = nn.AvgPool2d(2, stride=2)  # GAP layer
         self.ca = ChannelAttention(c2)
         self.sa = SpatialAttention()
         # self.conv_stride = nn.Conv2d(c2, c2, 3, 2)
