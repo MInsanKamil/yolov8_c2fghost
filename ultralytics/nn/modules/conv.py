8eee803f18f091e02d8b0ea1d12b9e8311e9ed14
MInsanKamil <89583296+MInsanKamil@users.noreply.github.com>
2024-06-09 17:07:22 +0800

    Down Attn

--- ultralytics/nn/modules/conv.py
+++ ultralytics/nn/modules/conv.py
@@ -1493,7 +1493,7 @@ class Concat_Feature_Map(nn.Module):
         """Concatenates a list of tensors along a specified dimension."""
         super().__init__()
         self.d = dimension
-        self.pool1 = nn.AvgPool2d(3,2,1)
+        self.pool1 = nn.AvgPool2d(2,2)
 
     def forward(self, x):
         """Forward pass for the YOLOv8 mask Proto module."""
