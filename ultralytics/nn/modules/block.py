1c3269a40fd555824f28742da82814c200791917
MInsanKamil <89583296+MInsanKamil@users.noreply.github.com>
2024-06-10 12:11:13 +0800

    ds conv

--- ultralytics/nn/modules/block.py
+++ ultralytics/nn/modules/block.py
@@ -604,8 +604,8 @@ class C2f_DS_Conv(nn.Module):
         """
         super().__init__()
         self.c = int(c2 * e)  # hidden channels
-        self.cv1 = Conv(c1, 2 * self.c, 1, 1)
-        self.cv2 = Conv((2 + n) * self.c, c2, 1)  # optional act=FReLU(c2)
+        self.cv1 = DS_Conv(c1, 2 * self.c, 1, 1)
+        self.cv2 = DS_Conv((2 + n) * self.c, c2, 1)  # optional act=FReLU(c2)
         self.m = nn.ModuleList(Bottleneck_DSConv(self.c, self.c, shortcut, g, k=((3, 3), (3, 3)), e=1.0) for _ in range(n))
 
     def forward(self, x):
