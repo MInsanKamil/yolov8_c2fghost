ab4af97842df16ae4996f47d96fe97391911a1bc
MInsanKamil <89583296+MInsanKamil@users.noreply.github.com>
2024-06-11 23:27:29 +0800

    ghost conv

--- ultralytics/nn/modules/conv.py
+++ ultralytics/nn/modules/conv.py
@@ -312,7 +312,7 @@ class GhostConv(nn.Module):
         super().__init__()
         c_ = c2 // 2  # hidden channels
         self.cv1 = Conv(c1, c_, k, s, None, g, act=act)
-        self.cv2 = Conv(c_, c_, 5, 5, None, c_, act=act)
+        self.cv2 = Conv(c_, c_, 5, 1, None, c_, act=act)
 
     def forward(self, x):
         """Forward propagation through a Ghost Bottleneck layer with skip connection."""
