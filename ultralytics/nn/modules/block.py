e3fe19fc13995c645331d277fe6761a9c6a9fae4
MInsanKamil <89583296+MInsanKamil@users.noreply.github.com>
2024-06-11 23:22:33 +0800

    up down

--- ultralytics/nn/modules/block.py
+++ ultralytics/nn/modules/block.py
@@ -972,9 +972,9 @@ class GhostBottleneck(nn.Module):
         super().__init__()
         c_ = c2 // 2
         self.conv = nn.Sequential(
-            GhostConv_Modification(c1, c_, 1, 1),  # pw
+            GhostConv(c1, c_, 1, 1),  # pw
             DWConv(c_, c_, k, s, act=False) if s == 2 else nn.Identity(),  # dw
-            GhostConv_Modification(c_, c2, 1, 1, act=False),  # pw-linear
+            GhostConv(c_, c2, 1, 1, act=False),  # pw-linear
         )
         self.shortcut = (
             nn.Sequential(DWConv(c1, c1, k, s, act=False), Conv(c1, c2, 1, 1, act=False)) if s == 2 else nn.Identity()
--- ultralytics/nn/modules/conv.py
+++ ultralytics/nn/modules/conv.py
@@ -312,7 +312,7 @@ class GhostConv(nn.Module):
         super().__init__()
         c_ = c2 // 2  # hidden channels
         self.cv1 = Conv(c1, c_, k, s, None, g, act=act)
-        self.cv2 = Conv(c_, c_, 1, 1, None, c_, act=act)
+        self.cv2 = Conv(c_, c_, 5, 5, None, c_, act=act)
 
     def forward(self, x):
         """Forward propagation through a Ghost Bottleneck layer with skip connection."""
@@ -330,7 +330,7 @@ class GhostConv_Modification(nn.Module):
         super().__init__()
         c_ = c2 // 2  # hidden channels
         self.cv1 = Conv(c1, c_, k, s, None, g, act=act)
-        self.cv2 = DS_Conv(c_, c_, 1, 1, None, act=act)  # adjust the number of output channels to c2
+        self.cv2 = Conv(c_, c_, 1, 1, None, c_, act=act)
 
     def forward(self, x):
         """Forward propagation through a Ghost Bottleneck layer with skip connection."""
