052d61332e88796a627b89a84d1a634dd688dde1
MInsanKamil <89583296+MInsanKamil@users.noreply.github.com>
2024-06-12 09:47:36 +0800

    slice samp

--- ultralytics/nn/modules/conv.py
+++ ultralytics/nn/modules/conv.py
@@ -289,7 +289,7 @@ class sliceSamp_Conv(nn.Module):
     def __init__(self, c1, c2, k=1, s=1, p=None, g=1, d=1, act=True):
         """Initialize Conv layer with given arguments including activation."""
         super().__init__()
-        self.conv = nn.Conv2d(c1 * 2, c2, k, s, autopad(k, p, d), groups=g, dilation=d, bias=False)
+        self.conv = nn.Conv2d(c1 * 2, c2 * 2, k, s, autopad(k, p, d), groups=g, dilation=d, bias=False)
         self.bn = nn.BatchNorm2d(c2)
         self.act = self.default_act if act is True else act if isinstance(act, nn.Module) else nn.Identity()
 
