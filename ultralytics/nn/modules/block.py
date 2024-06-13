87d6f2056bbbf954de716a1218e0a9bfa221fff6
MInsanKamil <89583296+MInsanKamil@users.noreply.github.com>
2024-06-13 19:27:26 +0800

    ghost conv

--- ultralytics/nn/modules/block.py
+++ ultralytics/nn/modules/block.py
@@ -6,7 +6,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torchvision
 
-from .conv import Conv,Conv_DownSampleAttn,Conv_Attn,DS_Conv,GhostConv_Modification, Conv_Down_Up,DWConv, GhostConv, LightConv, RepConv, autopad, Conv_Max_Pooling, Conv_SP, Conv_Prune
+from .conv import Conv,Conv_DownSampleAttn,Conv_Attn,DS_Conv,GhostConv_Modification, Conv_Down_Up,DWConv, GhostConv, LightConv, RepConv, autopad, Conv_Max_Pooling, Conv_SP, Conv_Prune, CBAM
 from .transformer import TransformerBlock
 
 __all__ = (
@@ -578,8 +578,8 @@ class C2f_Ghost_Conv(nn.Module):
         """
         super().__init__()
         self.c = int(c2 * e)  # hidden channels
-        self.cv1 = Conv(c1, 2 * self.c, 1, 1)
-        self.cv2 = Conv((2 + n) * self.c, c2, 1)  # optional act=FReLU(c2)
+        self.cv1 = GhostConv_Modification(c1, 2 * self.c, 1, 1)
+        self.cv2 = GhostConv_Modification((2 + n) * self.c, c2, 1)  # optional act=FReLU(c2)
         self.m = nn.ModuleList(GhostBottleneck_Real(self.c, self.c, shortcut, g, k=((3, 3), (3, 3)), e=1.0) for _ in range(n))
 
     def forward(self, x):
@@ -656,22 +656,23 @@ class C2f_sa(nn.Module):
         """
         super().__init__()
         self.c = int(c2 * e)  # hidden channels
-        self.cv1 = Conv_SP(c1, 2 * self.c, 1, 1)
-        self.cv2 = Conv_SP((2 + n) * self.c, c2, 1)  # optional act=FReLU(c2)
+        self.cv1 = Conv(c1, 2 * self.c, 1, 1)
+        self.cv2 = Conv((2 + n) * self.c, c2, 1)  # optional act=FReLU(c2)
         self.m = nn.ModuleList(Bottleneck(self.c, self.c, shortcut, g, k=((3, 3), (3, 3)), e=1.0) for _ in range(n))
+        self.attn = CBAM(c2)
 
     def forward(self, x):
         """Forward pass through C2f layer."""
         y = list(self.cv1(x).chunk(2, 1))
         y.extend(m(y[-1]) for m in self.m)
-        return self.cv2(torch.cat(y, 1))
+        return self.attn(self.cv2(torch.cat(y, 1)))
         
 
     def forward_split(self, x):
         """Forward pass using split() instead of chunk()."""
         y = list(self.cv1(x).split((self.c, self.c), 1))
         y.extend(m(y[-1]) for m in self.m)
-        return self.cv2(torch.cat(y, 1))
+        return self.attn(self.cv2(torch.cat(y, 1)))
     
 class C3f_Double_Stride(nn.Module):
     """Faster Implementation of CSP Bottleneck with 2 convolutions."""
