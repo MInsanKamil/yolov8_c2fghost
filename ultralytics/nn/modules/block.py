cf64269212a7e5259cdd8ca0c97b2b744e49c341
MInsanKamil <89583296+MInsanKamil@users.noreply.github.com>
2024-06-13 22:44:07 +0800

    ghost conv

--- ultralytics/nn/modules/block.py
+++ ultralytics/nn/modules/block.py
@@ -578,8 +578,8 @@ class C2f_Ghost_Conv(nn.Module):
         """
         super().__init__()
         self.c = int(c2 * e)  # hidden channels
-        self.cv1 = GhostConv_Modification(c1, 2 * self.c, 1, 1)
-        self.cv2 = GhostConv_Modification((2 + n) * self.c, c2, 1)  # optional act=FReLU(c2)
+        self.cv1 = Conv(c1, 2 * self.c, 1, 1)
+        self.cv2 = Conv((2 + n) * self.c, c2, 1)  # optional act=FReLU(c2)
         self.m = nn.ModuleList(GhostBottleneck_Real(self.c, self.c, shortcut, g, k=((3, 3), (3, 3)), e=1.0) for _ in range(n))
 
     def forward(self, x):
