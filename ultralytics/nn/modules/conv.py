a899b53bd04bad72a241d89cb72f015855f18bdc
MInsanKamil <89583296+MInsanKamil@users.noreply.github.com>
2024-06-13 16:11:19 +0800

    ghost conv

--- ultralytics/nn/modules/conv.py
+++ ultralytics/nn/modules/conv.py
@@ -842,7 +842,7 @@ class DS_Conv(nn.Module):
     def __init__(self, c1, c2, k=1, s=1, p=None, g=1, d=1, act=True):
         super(DS_Conv, self).__init__()
         self.depthwise = nn.Conv2d(c1, c2, k, s,padding=autopad(k, p, d), groups=math.gcd(c1, c2), dilation=d, bias=False)
-        self.pointwise = nn.Conv2d(c2, c2, kernel_size=3, bias=False)
+        self.pointwise = nn.Conv2d(c2, c2, kernel_size=3,padding=autopad(3, p, d), bias=True)
         self.bn = nn.BatchNorm2d(c2)
         self.act = self.default_act if act is True else act if isinstance(act, nn.Module) else nn.Identity()
     def forward(self, x):
