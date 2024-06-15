dd92ef11a371c2c513b38abc46967b3051df812e
MInsanKamil <89583296+MInsanKamil@users.noreply.github.com>
2024-06-15 18:21:34 +0800

    max-pooling dropout attn

--- ultralytics/nn/modules/conv.py
+++ ultralytics/nn/modules/conv.py
@@ -500,22 +500,22 @@ class Conv_Max_Pooling_Dropout_Attn(nn.Module):
         self.act = self.default_act if act is True else act if isinstance(act, nn.Module) else nn.Identity()
         self.max_pool = nn.MaxPool2d(3, stride=2)  # GAP layer
         self.dropout = nn.Dropout(0.2)
-        self.sa= SpatialAttention()
-        self.ca= ChannelAttention(c1)
+        # self.sa= SpatialAttention()
+        # self.ca= ChannelAttention(c1)
         
 
     def forward(self, x):
         """Apply convolution, batch normalization and activation to input tensor."""
-        x = self.ca(x)
+        # x = self.ca(x)
         x = self.act(self.bn(self.conv(self.max_pool(self.dropout(x)))))
-        x = self.sa(x)
+        # x = self.sa(x)
         return x
 
     def forward_fuse(self, x):
         """Perform transposed convolution of 2D data."""
-        x = self.ca(x)
+        # x = self.ca(x)
         x = self.act(self.conv(self.max_pool(self.dropout(x))))
-        x = self.sa(x)
+        # x = self.sa(x)
         return x
     
 class Avg_Pooling_Conv(nn.Module):
@@ -871,20 +871,20 @@ class Conv_Attn(nn.Module):
         self.bn = nn.BatchNorm2d(c2)
         self.act = self.default_act if act is True else act if isinstance(act, nn.Module) else nn.Identity()
         self.ca = ChannelAttention(c1)
-        self.sa = SpatialAttention()
+        # self.sa = SpatialAttention()
 
     def forward(self, x):
         """Apply convolution, batch normalization and activation to input tensor."""
         x = self.ca(x)
         x = self.act(self.bn(self.conv(x)))
-        x = self.sa(x)
+        # x = self.sa(x)
         return x
 
     def forward_fuse(self, x):
         """Perform transposed convolution of 2D data."""
         x = self.ca(x)
         x = self.act(self.conv(x))
-        x = self.sa(x)
+        # x = self.sa(x)
         return x
     
 class Conv_Dropout(nn.Module):
