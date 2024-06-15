f9c1c67b33604f13689e2bbd9ffd132e636bf059
MInsanKamil <89583296+MInsanKamil@users.noreply.github.com>
2024-06-15 20:20:53 +0800

    max-pooling dropout attn

--- ultralytics/nn/modules/conv.py
+++ ultralytics/nn/modules/conv.py
@@ -14,6 +14,7 @@ from ultralytics.utils import LOGGER
 
 __all__ = (
     "Conv",
+    "Conv_Max_Pooling_Dropout_Attn",
     "Conv2",
     "Nothing",
     "GhostConv_Attn",
@@ -502,6 +503,19 @@ class Conv_Max_Pooling_Dropout(nn.Module):
         self.dropout = nn.Dropout(0.15)
         
 
+class Conv_Max_Pooling_Dropout_Attn(nn.Module):
+    """Standard convolution with args(ch_in, ch_out, kernel, stride, padding, groups, dilation, activation)."""
+
+    default_act = nn.SiLU()  # default activation
+
+    def __init__(self, c1, c2, k=1, s=1, p=None, g=1, d=1, act=True):
+        """Initialize Conv layer with given arguments including activation."""
+        super().__init__()
+        self.conv = nn.Conv2d(c1, c2, k, s, autopad(k, p, d), groups=g, dilation=d, bias=False)
+        self.bn = nn.BatchNorm2d(c2)
+        self.act = self.default_act if act is True else act if isinstance(act, nn.Module) else nn.Identity()
+        self.max_pool = nn.MaxPool2d(3, stride=2)  # GAP layer
+        self.dropout = nn.Dropout(0.2)
     def forward(self, x):
         """Apply convolution, batch normalization and activation to input tensor."""
         x = self.act(self.bn(self.conv(self.max_pool(self.dropout(x)))))
