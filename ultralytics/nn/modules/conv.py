888b036137e01daf2480460acdfcb791561718d9
MInsanKamil <89583296+MInsanKamil@users.noreply.github.com>
2024-06-19 01:48:05 +0800

    max-pooling dropout attn

--- ultralytics/nn/modules/conv.py
+++ ultralytics/nn/modules/conv.py
@@ -571,7 +571,7 @@ class Conv_Max_Pooling_Dropout_Attn(nn.Module):
         self.act = self.default_act if act is True else act if isinstance(act, nn.Module) else nn.Identity()
         self.max_pool = nn.MaxPool2d(3, stride=2)  # GAP layer
         self.dropout = nn.Dropout(0.5)
-        self.sa= SpatialAttention()
+        self.sa= SpatialAttention(3)
         # self.ca= ChannelAttention(c1)
         
 
@@ -946,7 +946,7 @@ class Conv_Attn(nn.Module):
         self.conv = nn.Conv2d(c1, c2, k, s, autopad(k, p, d), groups=g, dilation=d, bias=False)
         self.bn = nn.BatchNorm2d(c2)
         self.act = self.default_act if act is True else act if isinstance(act, nn.Module) else nn.Identity()
-        self.sa = SpatialAttention()
+        self.sa = SpatialAttention(3)
 
     def forward(self, x):
         """Apply convolution, batch normalization and activation to input tensor."""
