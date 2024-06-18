a47a2246b4ccc591c208cc84168951eb283fa442
MInsanKamil <89583296+MInsanKamil@users.noreply.github.com>
2024-06-19 04:29:45 +0800

    knowledge distilation

--- ultralytics/nn/modules/conv.py
+++ ultralytics/nn/modules/conv.py
@@ -674,7 +674,7 @@ class Conv_Avg_Pooling(nn.Module):
         self.conv = nn.Conv2d(c1, c2, k, s, autopad(k, p, d), groups=g, dilation=d, bias=False)
         self.bn = nn.BatchNorm2d(c2)
         self.act = self.default_act if act is True else act if isinstance(act, nn.Module) else nn.Identity()
-        self.avg_pool = nn.AvgPool2d(2, stride=2)  # GAP layer
+        self.avg_pool = nn.AvgPool2d(3, stride=2)  # GAP layer
 
     def forward(self, x):
         """Apply convolution, batch normalization and activation to input tensor."""
