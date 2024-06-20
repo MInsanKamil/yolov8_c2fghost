5eea3fb19b71171dba2a885ed1665df3907ed636
MInsanKamil <89583296+MInsanKamil@users.noreply.github.com>
2024-06-20 08:07:39 +0800

    max-pooling dropout attn

--- ultralytics/nn/modules/block.py
+++ ultralytics/nn/modules/block.py
@@ -692,14 +692,16 @@ class C2f_sa(nn.Module):
         """Forward pass through C2f layer."""
         y = list(self.cv1(x).chunk(2, 1))
         y.extend(m(y[-1]) for m in self.m)
-        return self.attn(self.cv2(torch.cat(y, 1)))
+        z = self.cv2(torch.cat(y, 1))
+        return torch.cat((self.attn(z), z), 1)
         
 
     def forward_split(self, x):
         """Forward pass using split() instead of chunk()."""
         y = list(self.cv1(x).split((self.c, self.c), 1))
         y.extend(m(y[-1]) for m in self.m)
-        return self.attn(self.cv2(torch.cat(y, 1)))
+        z = self.cv2(torch.cat(y, 1))
+        return torch.cat((self.attn(z), z), 1)
     
 class C3f_Double_Stride(nn.Module):
     """Faster Implementation of CSP Bottleneck with 2 convolutions."""
