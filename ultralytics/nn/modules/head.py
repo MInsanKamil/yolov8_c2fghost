5200a80d7e3e2c4a9673500ca5d26b9e5b10873c
MInsanKamil <89583296+MInsanKamil@users.noreply.github.com>
2024-06-20 08:21:39 +0800

    max-pooling dropout attn

--- ultralytics/nn/modules/head.py
+++ ultralytics/nn/modules/head.py
@@ -35,9 +35,9 @@ class Detect(nn.Module):
         self.stride = torch.zeros(self.nl)  # strides computed during build
         c2, c3 = max((16, ch[0] // 4, self.reg_max * 4)), max(ch[0], min(self.nc, 100))  # channels
         self.cv2 = nn.ModuleList(
-            nn.Sequential(GhostConv_Modification(x*2, c2, 3), GhostConv_Modification(c2, c2, 3), nn.Conv2d(c2, 4 * self.reg_max, 1)) for x in ch
+            nn.Sequential(GhostConv_Modification(x, c2, 3), GhostConv_Modification(c2, c2, 3), nn.Conv2d(c2, 4 * self.reg_max, 1)) for x in ch
         )
-        self.cv3 = nn.ModuleList(nn.Sequential(GhostConv_Modification(x*2, c3, 3), GhostConv_Modification(c3, c3, 3), nn.Conv2d(c3, self.nc, 1)) for x in ch)
+        self.cv3 = nn.ModuleList(nn.Sequential(GhostConv_Modification(x, c3, 3), GhostConv_Modification(c3, c3, 3), nn.Conv2d(c3, self.nc, 1)) for x in ch)
         self.dfl = DFL(self.reg_max) if self.reg_max > 1 else nn.Identity()
         # self.up = nn.Upsample(scale_factor=2, mode="nearest")
 
