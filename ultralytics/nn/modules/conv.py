0c3e912677024c170196cf9dbd85343415b69109
MInsanKamil <89583296+MInsanKamil@users.noreply.github.com>
2024-06-24 23:52:05 +0800

    max-pooling dropout attn

--- ultralytics/nn/modules/conv.py
+++ ultralytics/nn/modules/conv.py
@@ -502,7 +502,7 @@ class Conv_Max_Pooling_Dropout(nn.Module):
         self.bn = nn.BatchNorm2d(c2)
         self.act = self.default_act if act is True else act if isinstance(act, nn.Module) else nn.Identity()
         self.max_pool = nn.MaxPool2d(3, stride=2)  # GAP layer
-        self.dropout = nn.Dropout(0.2)
+        self.dropout = nn.Dropout(0.5)
 
         # self.sa= SpatialAttention()
         # self.ca= ChannelAttention(c1)
@@ -614,28 +614,24 @@ class Conv_Max_Pooling_Dropout_Attn(nn.Module):
         self.act = self.default_act if act is True else act if isinstance(act, nn.Module) else nn.Identity()
         self.max_pool = nn.MaxPool2d(3, stride=2)  # GAP layer
         self.dropout = nn.Dropout(0.5)
-        self.sa= SpatialAttention()
+        # self.sa= SpatialAttention()
         self.ca= ChannelAttention(c2)
         
 
     def forward(self, x):
         """Apply convolution, batch normalization and activation to input tensor."""
         if self.training:   
-            x = self.act(self.bn(self.conv(self.max_pool(self.dropout(x)))))
+            x = self.act(self.bn(self.ca(self.conv(self.max_pool(self.dropout(x))))))
         else:
-            x = self.act(self.bn(self.conv(self.max_pool(x))))
-        x = self.ca(x)
-        x = self.sa(x)
+            x = self.act(self.bn(self.ca(self.conv(self.max_pool(x)))))
         return x
 
     def forward_fuse(self, x):
         """Perform transposed convolution of 2D data."""
         if self.training:   
-            x = self.act((self.conv(self.max_pool(self.dropout(x)))))
+            x = self.act(self.ca(self.conv(self.max_pool(self.dropout(x)))))
         else:
-            x = self.act((self.conv(self.max_pool(x))))
-        x = self.ca(x)
-        x = self.sa(x)
+            x = self.act(self.ca(self.conv(self.max_pool(x))))
         return x
     
 class Avg_Pooling_Conv(nn.Module):
@@ -1008,20 +1004,15 @@ class Conv_Attn(nn.Module):
         self.bn = nn.BatchNorm2d(c2)
         self.act = self.default_act if act is True else act if isinstance(act, nn.Module) else nn.Identity()
         self.ca = ChannelAttention(c2)
-        self.sa = SpatialAttention()
 
     def forward(self, x):
         """Apply convolution, batch normalization and activation to input tensor."""
-        x = self.act((self.bn(self.conv(x))))
-        x = self.ca(x)
-        x = self.sa(x)
+        x = self.act((self.bn(self.ca(self.conv(x)))))
         return x
 
     def forward_fuse(self, x):
         """Perform transposed convolution of 2D data."""
-        x = self.act((self.conv(x)))
-        x = self.ca(x)
-        x = self.sa(x)
+        x = self.act(self.ca(self.conv(x)))
         return x
     
 class Conv_Dropout(nn.Module):
