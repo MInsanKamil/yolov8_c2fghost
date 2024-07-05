dcacd9331976984af61466f5ca473a90d53d4444
MInsanKamil <89583296+MInsanKamil@users.noreply.github.com>
2024-07-06 00:04:58 +0800

    head ghost module

--- ultralytics/nn/modules/head.py
+++ ultralytics/nn/modules/head.py
@@ -37,9 +37,9 @@ class Detect(nn.Module):
         c2, c3 = max((16, ch[0] // 4, self.reg_max * 4)), max(ch[0], min(self.nc, 100))  # channels
         # LOGGER.info(f"ch[0] {ch[0]}")
         self.cv2 = nn.ModuleList(
-            nn.Sequential(Conv(x, c2, 3), Conv(c2, c2, 3),nn.Conv2d(c2, 4 * self.reg_max, 1)) for x in ch
+            nn.Sequential(GhostConv(x, c2, 3), GhostConv(c2, c2, 3),nn.Conv2d(c2, 4 * self.reg_max, 1)) for x in ch
         )
-        self.cv3 = nn.ModuleList(nn.Sequential(Conv(x, c3, 3), Conv(c3, c3, 3), nn.Conv2d(c3, self.nc, 1)) for x in ch)
+        self.cv3 = nn.ModuleList(nn.Sequential(GhostConv(x, c3, 3), GhostConv(c3, c3, 3), nn.Conv2d(c3, self.nc, 1)) for x in ch)
         self.dfl = DFL(self.reg_max) if self.reg_max > 1 else nn.Identity()
         # self.up = nn.Upsample(scale_factor=2, mode="nearest")
 
