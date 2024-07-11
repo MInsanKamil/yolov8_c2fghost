6dbb9fadfa65e3c9974f67916a5871a8ff060fb9
MInsanKamil <89583296+MInsanKamil@users.noreply.github.com>
2024-07-11 09:43:30 +0800

    avg pooling

--- ultralytics/nn/modules/conv.py
+++ ultralytics/nn/modules/conv.py
@@ -383,7 +383,7 @@ class MaxAvg_Pooling_Conv(nn.Module):
         LOGGER.info(f"shape input{x.size()}")
         LOGGER.info(f"shape after avg pooling{self.avg_pool(x).size()}")
         LOGGER.info(f"shape after max pooling{self.max_pool(x).size()}")
-        LOGGER.info(f"shape after concat {torch.cat((self.avg_pool(x), self.max_pool(x)), 1)}")
+        LOGGER.info(f"shape after concat {torch.cat((self.avg_pool(x), self.max_pool(x)), 1).size()}")
         return self.act(self.bn(self.conv(torch.cat((self.avg_pool(x), self.max_pool(x)), 1))))
 
     def forward_fuse(self, x):
