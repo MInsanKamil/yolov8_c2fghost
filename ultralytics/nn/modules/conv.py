1a3425ede67ccf210e0f77434bdbc3d316d6e2cd
MInsanKamil <89583296+MInsanKamil@users.noreply.github.com>
2024-07-12 09:43:45 +0800

    max-pooling dropout

--- ultralytics/nn/modules/conv.py
+++ ultralytics/nn/modules/conv.py
@@ -581,18 +581,18 @@ class Conv_Max_Pooling(nn.Module):
         self.conv = nn.Conv2d(c1, c2, k, s, autopad(k, p, d), groups=g, dilation=d, bias=False)
         self.bn = nn.BatchNorm2d(c2)
         self.act = self.default_act if act is True else act if isinstance(act, nn.Module) else nn.Identity()
-        self.max_pool = nn.MaxPool2d(3, stride=2)  # GAP layer
+        self.max_pool = nn.MaxPool2d(2, stride=2)  # GAP layer
         
     def forward(self, x):
         """Apply convolution, batch normalization and activation to input tensor."""
-        x = self.act(self.bn(self.conv(x)))
         x = self.max_pool(x)
+        x = self.act(self.bn(self.conv(x)))
         return x
 
     def forward_fuse(self, x):
         """Perform transposed convolution of 2D data."""
-        x = self.act(self.conv(x))
         x = self.max_pool(x)
+        x = self.act(self.conv(x))
         return x
     
 class Conv_Weighted_Pooling(nn.Module):
